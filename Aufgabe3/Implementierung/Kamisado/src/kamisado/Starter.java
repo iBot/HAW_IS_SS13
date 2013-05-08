509b18a4b87f912faac82f1a63beaa62286358b5
iBot <iBot@no.mail>
2013-05-08 11:26:32 +0200

    Los geht's...

---
+++ Aufgabe3/Implementierung/Kamisado/src/kamisado/Starter.java
@@ -0,0 +1,15 @@
+package kamisado;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: tobi
+ * Date: 08.05.13
+ * Time: 11:24
+ * To change this template use File | Settings | File Templates.
+ */
+public class Starter {
+
+    public static void main(String[] args){
+        //TODO: Implement all the stuff...
+    }
+}
