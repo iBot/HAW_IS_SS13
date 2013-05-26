eab971ac3e17d26311afe2b57abd07cc688b56ca
iBot <iBot@no.mail>
2013-05-26 13:48:02 +0200

    Projekt für Sim-Spiel angelegt.

---
+++ Aufgabe3/Implementierung/Sim/src/application/Starter.java
@@ -0,0 +1,15 @@
+package application;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: tobi
+ * Date: 26.05.13
+ * Time: 13:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class Starter {
+
+    private static void main(String[] args){
+        //TODO: Start game...
+    }
+}
